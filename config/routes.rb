7d5a7120df69a5238f35f3bce9733589054c7063
renren121314 <renren199612@gmail.com>
2022-10-12 12:29:20 +0000

    chapter13

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'homes/about' ,as:'about'
   devise_for :users
-  resources :post_images, only: [:new, :create, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
 
 
 
